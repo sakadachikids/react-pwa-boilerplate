7b23da16428274d639ab3a9f2b4c8c4fcbf7a8d1
Takahiro Matsuda <takakobem@gmail.com>
2019-04-28 16:38:20 +0900

    eslintを追加

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint"
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  env: { browser: true, node: true, es6: true },
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.json"
+  },
+  rules: {}
+};
