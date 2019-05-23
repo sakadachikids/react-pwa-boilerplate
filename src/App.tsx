a09971e31be09f739a3163b69c20e005fe4bbe7e
Ryota.Kunisada <r.kunisada661@gmail.com>
2019-05-24 00:39:02 +0900

    Drawer追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
+import PhotoCamera from '@material-ui/icons/PhotoCamera'
+import Mic from '@material-ui/icons/Mic'
+import SettingsIcon from '@material-ui/icons/Settings'
 
 const App: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false)
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <SwipeableDrawer
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        onOpen={() => setIsOpen(true)}
+      >
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <PhotoCamera />
+            </ListItemIcon>
+            <ListItemText>Photo</ListItemText>
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <Mic />
+            </ListItemIcon>
+            <ListItemText>Record</ListItemText>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText>Settings</ListItemText>
+          </ListItem>
+        </List>
+      </SwipeableDrawer>
     </div>
   )
 }
